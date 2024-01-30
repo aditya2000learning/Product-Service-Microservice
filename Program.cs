b17073ba243d7579b5eb0a44069c4dfa4cf1642b
Aditya <aditya2000learning@gmail.com>
2024-01-31 01:34:01 +0530

    port

--- Program.cs
+++ Program.cs
@@ -3,6 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddSingleton<List<Product>>();
 
+// Add the following for Kubernetes Deployment
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+var url = $"http://0.0.0.0:{port}";
+builder.WebHost.UseUrls(url);
+
 var app = builder.Build();
 
 // Seed initial data
